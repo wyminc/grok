c93debaf2fbaa3d1e3079b497c857a11f7ff8c6b
Harsh Kotak <kotak_harsh@hotmail.com>
2018-11-28 19:10:53 -1000

    ✨ router structure

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <h1>Alohaaaaaa</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
