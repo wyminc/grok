4ed27af3c7dd08f3fe77deb040972fb43b0878ea
Harsh Kotak <kotak_harsh@hotmail.com>
2018-11-29 18:27:36 -1000

    💄 implemented sass

--- src/containers/Footer/FooterComponent.jsx
+++ src/containers/Footer/FooterComponent.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './styles.css';
+import './styles.scss';
 
 class Footer extends Component {
   render() {
--- src/containers/home/FeaturedCardsComponent.jsx
+++ src/containers/home/FeaturedCardsComponent.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './styles.css';
+import './styles.scss';
 
 import { CardOne, CardTwo } from '../../components/CardComponent.jsx'
 
@@ -12,6 +12,7 @@ class FeaturedCardsComponent extends Component {
 
   componentDidMount() {
     console.log('didMount Hits');
+    console.log(React.version)
   }
 
   render() {
