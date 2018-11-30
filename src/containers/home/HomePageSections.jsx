9ed586da1b1533dfae7cea969df458ccd4983639
Sarah Yamashige <sarahyamashige@Sarahs-MBP.home>
2018-11-30 01:06:38 -1000

    adding sections to the home page

---
+++ src/containers/home/HomePageSections.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './styles.css';
+
+export const AboutSectionComponent = () => {
+    return (
+        <div>Testing</div>
+    )
+}
+
+export const PremiumSectionComponent = () => {
+    return (
+        <div className="premium-section-container">
+            <div className="plans">
+                <div className="basic-plan">
+                    <div className="plan-image"></div>
+                </div>
+                <div className="basic-plan plan-info">
+                    <h4>Basic Grok Plan</h4>
+                    <ul>
+                        <li>something</li>
+                        <li>about</li>
+                        <li>this</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="plans">
+                <div className="fancy-plan">
+                    <div className="plan-image"></div>
+                </div>
+                <div className="fancy-plan plan-info">
+                    <h4>Grok It Up A Notch</h4>
+                </div>
+            </div>
+            <div className="plans">
+                <div className="extra-fancy-plan">
+                    <div className="plan-image"></div>
+                </div>
+                <div className="extra-fancy-plan plan-info">
+                    <h4>Super Grok'n Fancy</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
