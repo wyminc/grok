1dbdc1a590b03f8378b6d9aa7625ae75f3dd94ec
Sarah Yamashige <sarahyamashige@Sarahs-MBP.home>
2018-11-29 21:53:30 -1000

    Added styling to three of the home page sample cards

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -3,14 +3,17 @@ import './styles.css'
 
 export const CardOne = () => {
   return (
-    <div key="card" className="CardOne" className="card">
-      <div className="card-front">
+    <div key="card" className="CardOne card">
+      <div className="card-front one-front">
+        <div className="one-title">COMPANY NAME</div>
       </div>
-      <div className="card-info">
-        <div className="name">made it.</div>
-        <div className="address">Address</div>
-        <div className="phoneNumber">Phone #</div>
-        <div className="email">eMail</div>
+      <div className="card-info one-back">
+        <div className="one-info">
+          <div className="name">Company Name</div>
+          <div className="address">Address <br/> City, State, Zip Code</div>
+          <div className="phoneNumber">Phone No.</div>
+          <div className="email">eMail</div>
+        </div>
       </div>
     </div>
   )
@@ -18,14 +21,17 @@ export const CardOne = () => {
 
 export const CardTwo = () => {
   return (
-    <div key="card" className="CardTwo" className="card" >
-      <div className="card-front">
+    <div key="card" className="CardTwo card">
+      <div className="card-front two-front">
+        <div className="two-title">COMPANY NAME</div>
       </div>
-      <div className="card-info">
-        <div className="name">Business Name</div>
-        <div className="address">Address</div>
-        <div className="phoneNumber">Phone #</div>
-        <div className="email">eMail</div>
+      <div className="card-info two-back">
+        <div className="two-info">
+          <div className="name">Company Name</div>
+          <div className="address">Address <br/> City, State, Zip Code</div>
+          <div className="phoneNumber">Phone No.</div>
+          <div className="email">eMail</div>
+        </div>
       </div>
     </div>
   )
@@ -33,14 +39,17 @@ export const CardTwo = () => {
 
 export const CardThree = () => {
   return (
-    <div key="card" className="CardThree" className="card">
-      <div className="card-front">
+    <div key="card" className="CardThree card">
+      <div className="card-front three-front">
+        <div className="three-title">COMPANY NAME</div>
       </div>
-      <div className="card-info">
-        <div className="name">Business Name</div>
-        <div className="address">Address</div>
-        <div className="phoneNumber">Phone #</div>
-        <div className="email">eMail</div>
+      <div className="card-info three-back">
+        <div className="three-info">
+            <div className="name">Company Name</div>
+            <div className="address">Address <br/> City, State, Zip Code</div>
+            <div className="phoneNumber">Phone No.</div>
+            <div className="email">eMail</div>
+          </div>
       </div>
     </div>
   )
@@ -48,8 +57,9 @@ export const CardThree = () => {
 
 export const CardFour = () => {
   return (
-    <div key="card" className="CardFour" className="card">
-      <div className="card-front">
+    <div key="card" className="CardFour card">
+      <div className="card-front four-front">
+        <div className="title">COMPANY NAME</div>
       </div>
       <div className="card-info">
         <div className="name">Business Name</div>
