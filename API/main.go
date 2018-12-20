087cfb6b13e2b257b402bfef09e37b79bd47dd31
Wymin Chan <wyminc808@gmail.com>
2018-12-19 21:37:22 -1000

    Using env file in docker container

--- API/main.go
+++ API/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"./server"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// err := godotenv.Load("../.env")
+	err := godotenv.Load("./.env")
 	router := gin.Default()
 
 	router.Use(cors.Default())
 
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	router.GET("/specific/:id", server.Card)    //Card by specific id
 	router.GET("/all/:id", server.AllCards)     //All cards in the wallet of id
@@ -23,5 +27,5 @@ func main() {
 	router.PUT("/update/:id", server.Update)    //Update card
 	router.DELETE("/delete/:id", server.Delete) //"Delete" card
 
-	router.Run(":8000")
+	router.Run(":" + os.Getenv("PORT"))
 }
